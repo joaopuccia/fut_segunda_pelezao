0f1fc3dfd0f96ab65a71149cb74d7f62c6840dcd
joaopuccia <39597575+joaopuccia@users.noreply.github.com>
2026-02-02 14:57:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -278,5 +278,8 @@ def create_app():
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    host = "0.0.0.0"
+    port = int(os.environ.get("PORT", "5000"))  # a plataforma define PORT
+    app.run(host=host, port=port, debug=False)
